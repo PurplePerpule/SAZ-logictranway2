2c6709f01f420647c975008707e2cba9959ad812
Purple <dhakut@yandex.ru>
2025-12-04 09:20:30 +0300

    api update

--- backend/app.py
+++ backend/app.py
@@ -19,7 +19,7 @@ import os
 
 
 app = Flask(__name__)
-CORS(app)
+CORS(app, supports_credentials=True)
 
 
 app.config["SECRET_KEY"] = "c639183901c409352be3d01c521c7694"
@@ -796,4 +796,4 @@ if __name__ == "__main__":
 
         db.create_all()
 
-    app.run(debug=True, port=5000)
+    app.run(host="0.0.0.0", port=5000, debug=True)
--- src/admin.js
+++ src/admin.js
@@ -1,5 +1,5 @@
 // @ts-nocheck
-const API = "http://192.168.15.189:5000";
+const API = "http://TransLogicSaz:5000";
 
 async function loadOrders() {
   try {
